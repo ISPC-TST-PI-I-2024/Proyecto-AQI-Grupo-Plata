3e6c3503cb24570a3108eea3e39b221dde06af82
karina-lolita <jazminbarbero0@gmail.com>
2024-09-07 22:06:04 -0300

    Actualización de archivos del sensor ML8511 y README

--- test/Test-ML8511-sensor/test/ML8511.cpp
+++ test/Test-ML8511-sensor/test/ML8511.cpp
@@ -11,8 +11,18 @@ float ML8511Sensor::readUV() {
     int uvLevel = analogRead(uvPin);
     int refLevel = analogRead(ref3V3Pin);
 
+    // Verificar que refLevel no sea cero para evitar división por cero
+    if (refLevel == 0) {
+        return NAN; // Retorna NaN si hay un error
+    }
+
     float outputVoltage = 3.3 / refLevel * uvLevel;
-    float uvIntensity = map(outputVoltage, 0.99, 2.9, 0.0, 15.0);
+    float uvIntensity = mapFloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
 
     return uvIntensity;
+}
+
+// Función personalizada para mapear valores flotantes
+float ML8511Sensor::mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
--- test/Test-ML8511-sensor/test/ML8511.h
+++ test/Test-ML8511-sensor/test/ML8511.h
@@ -12,6 +12,7 @@ public:
 private:
     int uvPin;
     int ref3V3Pin;
+    float mapFloat(float x, float in_min, float in_max, float out_min, float out_max); // Declaración de la función mapFloat
 };
 
 #endif
